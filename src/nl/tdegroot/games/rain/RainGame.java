34e6d1185aa9931daa106ba2dd3023a5e4354fc9
Timon de Groot <tdegroot96@gmail.com>
2015-01-14 22:35:17 +0100

    Forgot RainGame.java

---
+++ src/nl/tdegroot/games/rain/RainGame.java
@@ -0,0 +1,68 @@
+package nl.tdegroot.games.rain;
+
+import nl.tdegroot.games.pixxel.PixxelGame;
+import nl.tdegroot.games.pixxel.gfx.Screen;
+import nl.tdegroot.games.pixxel.gfx.Sprite;
+import nl.tdegroot.games.pixxel.util.Log;
+import nl.tdegroot.games.pixxel.util.Random;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RainGame extends PixxelGame {
+
+    private int spawnTimer;
+    private int rainDropCount = 6;
+    private List<RainDrop> rainDrops = new ArrayList<RainDrop>();
+
+    public RainGame(String title, int width, int height, int scale) {
+        super(title, width, height, scale);
+    }
+
+    public void init() {
+
+    }
+
+    public void tick(int delta) {
+        spawnTimer -= delta;
+        if (spawnTimer <= 0) {
+            for (int i = 0; i < rainDropCount; i++) {
+                int x = Random.nextInt(0, window.width / window.scale);
+                int y = -50;
+                double speed = Random.nextDouble();
+                int color = Random.nextInt(0xFF000055, 0xFF0000FF);
+                width = Random.nextInt(2, 6);
+                height = Random.nextInt(10, 14);
+                RainDrop rainDrop = new RainDrop(x, y, width, height, speed, color);
+                rainDrops.add(rainDrop);
+            }
+            spawnTimer = 2000;
+        }
+
+        if (rainDrops.size() <= 0) return;
+
+        for(int i = 0; i < rainDrops.size(); i++) {
+            if (rainDrops.get(i).finished) {
+                rainDrops.remove(i);
+                continue;
+            }
+            RainDrop drop = rainDrops.get(i);
+            drop.tick(delta / 200);
+            if (drop.x >= window.width / window.scale || drop.y >= window.height / window.scale)
+                drop.finished = true;
+        }
+    }
+
+    public void render() {
+        Screen screen = window.screen;
+        for(RainDrop drop : rainDrops) {
+            drop.render(screen);
+        }
+    }
+
+    public static void main(String[] args) {
+        RainGame game = new RainGame("Make it rain!", 1280, 720, 8);
+        game.start();
+    }
+
+}
